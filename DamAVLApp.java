a1a66ad7ca9d0f599af2757a2f05959077406ad1
Oyama Plati <platioyama@gmail.com>
2018-03-19 20:58:47 +0200

    New Feature

--- DamAVLApp.java
+++ DamAVLApp.java
@@ -17,11 +17,37 @@ public class DamAVLApp {
       inputStream = new Scanner(
          new FileInputStream(
            (new File("Dam_Levels_Individual_Nov2015-Mar2016.csv")).getAbsolutePath()));
-                                                          }
+    }
     catch (FileNotFoundException e) {
       e.printStackTrace();
       System.out.println("FAIL!! : File does not exist");
       System.out.println(" or could not be open.");
+    }
+    
+    String line = null;
+    
+    inputStream.nextLine();
+    
+    while ( inputStream.hasNextLine() ) {
+        
+              while (inputStream.hasNextLine()) {
+                       line = inputStream.nextLine();  
+                                
+                                         String[] rawdata = line.split(",");
+                                                  
+                                                           if (rawdata.length >= 26) {
+                                                                       bst.insert(
+                                                                                      new DamData(rawdata[2], rawdata[10], rawdata[25]));
+                                                                                               }
+                                                                                                        else if ((rawdata.length >= 11)
+                                                                                                                    && (rawdata.length < 26)) { 
+                                                                                                                                bst.insert(
+                                                                                                                                               new DamData(rawdata[2], rawdata[10], null));
+                                                                                                                                                        }
+                                                                                                                                                                 else if (rawdata.length < 11){
+                                                                                                                                                                             bst.insert(new DamData(rawdata[2], null, null));
+                                                                                                                                                                                      }    
+                                                                                                                                                                                            }  
     }
   }
 }
